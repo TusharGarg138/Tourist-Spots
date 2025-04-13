c71b0c3da6c22704ffbf07c0bf3dc2ddca26378f
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-04-13 15:23:36 +0530

    Stabilizing the connection between SQL and JAva

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,5 +1,9 @@
 package src;
 
 public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/tourism_planner";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "YOUR_DBMS_PASSWORD"; // replace with your actual password
+
     
 }
