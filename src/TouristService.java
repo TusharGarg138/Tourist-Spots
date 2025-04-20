3c2fccf316b9c92f31f5daacd957006afefce9d8
trijal-singh-babbar <trijalsinghbabbar@gmail.com>
2025-04-20 15:27:39 +0530

    Updated TouristService class

--- src/TouristService.java
+++ src/TouristService.java
@@ -5,12 +5,10 @@ import java.util.List;
 public class TouristService {
     private DatabaseManager dbManager;
 
-    // Constructor
     public TouristService() {
         dbManager = new DatabaseManager();
     }
 
-    // Gets tourist spots for a given city by calling the DB layer
     public List<TouristSpot> getSpotsForCity(String city) {
         return dbManager.getSpotsByCity(city);
     }
