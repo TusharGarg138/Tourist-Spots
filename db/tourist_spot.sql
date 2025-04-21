ba75c079794d4ffd4c6b93060a0c2e65d2dff58d
varun mate <varunmate4@gmail.com>
2025-04-21 20:33:29 +0530

    table tourist spots

--- db/tourist_spot.sql
+++ db/tourist_spot.sql
@@ -13,3 +13,11 @@ city_name VARCHAR(50) NOT NULL,
 state_id INT,
 FOREIGN KEY (state_id) REFERENCES states(state_id)
 );
+
+CREATE TABLE tourist_spots (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+city_id INT,
+description TEXT,
+FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
