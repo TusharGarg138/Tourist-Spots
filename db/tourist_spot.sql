414982dfb36bcb0e0887812bcc8b821d15bf4a89
varun mate <varunmate4@gmail.com>
2025-04-21 20:31:38 +0530

    creating city table

--- db/tourist_spot.sql
+++ db/tourist_spot.sql
@@ -6,3 +6,10 @@ CREATE TABLE states (
 state_id INT AUTO_INCREMENT PRIMARY KEY,
 state_name VARCHAR(50) NOT NULL UNIQUE
 );
+
+CREATE TABLE cities (
+city_id INT AUTO_INCREMENT PRIMARY KEY,
+city_name VARCHAR(50) NOT NULL,
+state_id INT,
+FOREIGN KEY (state_id) REFERENCES states(state_id)
+);
