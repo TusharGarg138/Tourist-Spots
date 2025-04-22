e981bc89f0d1bbeddb1062856180d2d0fea95326
shriyans009 <shriyanssahoo2005@gmail.com>
2025-04-22 05:43:34 +0530

    Updated DatabaseManager

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/tourism";
     private static final String USER = "root";
-    private static final String PASSWORD = "shriyans09";
+    private static final String PASSWORD = "password";
 
     public List<TouristSpot> getSpotsByCity(String cityName) throws DatabaseConnectionException {
         List<TouristSpot> spots = new ArrayList<>();
